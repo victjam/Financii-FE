f94b5eb058af320d17e0f1de5d86bee929599d95
victjam <victorjmanrique@gmail.com>
2024-04-18 11:47:19 -0500

    Update vite.config.ts to use alias for '@' path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,8 +7,6 @@ export default defineConfig({
     port: 4000,
   },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: [{ find: '@', replacement: '/src' }],
   },
 });
